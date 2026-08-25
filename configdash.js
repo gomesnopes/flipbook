771900f415a4015a57f0f3f6f3dbe445c49f6fad
gomesnopes <fandigomes.abk@gmail.com>
2026-08-25 18:20:05 +0900

    Update configdash.js

--- configdash.js
+++ configdash.js
@@ -1,4 +1,4 @@
-const supabaseUrl = 'https://zqjgimorpkzynytrhyde.supabase.co'; // Ganti dengan URL aslimu
+const supabaseUrl = 'https://zqjgimorpkzynytrhyde.supabase.co'; // Ganti dengan URL asli
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxamdpbW9ycGt6eW55dHJoeWRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwMzkyOTUsImV4cCI6MjA5NDYxNTI5NX0.LAbAMRLiu9XxiAxFvoSX9FhraFgXoCp0GkEZUlUhHow';     // Ganti dengan ANON KEY aslimu
 
 // Kita simpan ke dalam window agar bisa diakses global oleh file HTML
